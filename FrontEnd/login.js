81d879d588cc07dac8d6b07053575c236d964f73
Mourad Amessis <epaum6@gmail.com>
2023-07-15 23:24:12 +0200

    derniers changement

---
+++ FrontEnd/login.js
@@ -0,0 +1,31 @@
+//LOGIN
+function loginTest(){
+    const loginForm = document.querySelector(".form");
+    loginForm.addEventListener("submit", async function (event){
+        event.preventDefault();
+
+        const loginInfo = {
+            email: event.target.querySelector("[name=email]").value,
+            password: event.target.querySelector("[name=password]").value
+        };
+        const chargeUtile = JSON.stringify(loginInfo);
+
+      fetch("http://localhost:5678/api/users/login",{
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: chargeUtile,
+        }).then(response => response.json())
+        .then(data => {
+            if (data.userId && data.token){
+                const userId = data.userId;
+                const token = data.token;
+                localStorage.setItem('token', token);
+                window.location.href='index.html'
+            }else{
+                const message = data.message;
+                alert(message);
+            }
+        });
+    });
+}
+loginTest();
